d0943ef0e92bc59485459de12efd776e1e8e9c4d
Isaac Dunham <idunham@lavabit.com>
2012-05-15 09:21:00 -0700

    Get ready for abstractions

--- make.sh
+++ make.sh
@@ -3,19 +3,24 @@
 # Configuration
 . control.env
 export PATH=${BINDIR}:${PATH}
-egrep  ^[A-Z_]*= control.env |awk -F= '{print $1}' | xargs export 
+export `egrep  ^[A-Z_]*= control.env |awk -F= '{print $1}' | xargs`
 export CFLAGS="-fno-stack-protector"
 
 musl(){
-	export CC=${HOSTCC}
-	export CFLAGS="-Os -fno-stack-protector"
+	
+	export CC_MUSL=${HOSTCC}
+	export CFLAGS_MUSL="-Os -fno-stack-protector"
+	export package=musl
+	export PKG=MUSL
+	export CONF_MUSL="./configure --prefix=$PREFIX --bindir=$BINDIR --enable-gcc-wrapper"
+	. methods/vars.sh
 	case $1 in
 	get)
-		[ -d musl/.git ] || git clone "$MIR_MUSL" musl
+		[ -d musl/.git ] || git clone "$MIR_PKG" musl
 		;;
 	pull)
-		cd musl && git checkout "$BRN_MUSL" && \
-		  git pull "$MIR_MUSL" "$BRN_MUSL"
+		cd musl && git checkout "$BRN_PKG" && \
+		  git pull "$MIR_PKG" "$BRN_PKG"
 		cd -
 		;;
 	conf)
