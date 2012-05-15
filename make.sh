aab236cafd05a2ff93e2572abd5d61788b5b008c
Isaac Dunham <idunham@lavabit.com>
2012-05-15 09:43:41 -0700

    Use the git abstractions

--- make.sh
+++ make.sh
@@ -8,36 +8,13 @@ export CFLAGS="-fno-stack-protector"
 
 musl(){
 	
-	export CC_MUSL=${HOSTCC}
-	export CFLAGS_MUSL="-Os -fno-stack-protector"
+	export CC_MUSL=gcc-4.1
+	export CFLAGS_MUSL="-O3 -fno-stack-protector"
 	export package=musl
 	export PKG=MUSL
 	export CONF_MUSL="./configure --prefix=$PREFIX --bindir=$BINDIR --enable-gcc-wrapper"
 	. methods/vars.sh
-	case $1 in
-	get)
-		[ -d musl/.git ] || git clone "$MIR_PKG" musl
-		;;
-	pull)
-		cd musl && git checkout "$BRN_PKG" && \
-		  git pull "$MIR_PKG" "$BRN_PKG"
-		cd -
-		;;
-	conf)
-		musl get && cd musl && [ -e config.mak ] || \
-		./configure --prefix=${PREFIX} --bindir=${BINDIR} \
-		--enable-gcc-wrapper
-		cd -
-		;;
-	build)
-		musl conf && cd musl && make clean && make
-		cd -
-		;;
-	install)
-		musl build && cd musl && sudo make install
-		cd -
-		;;
-	esac
+	. methods/git.sh
 }
 
 musl build
